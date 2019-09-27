3ffd06eb7402bb2f596538a72ffb5a65d169a140
Joshua Belden <jcbelden@procaresoftware.com>
2019-09-27 13:13:55 -0700

    Added navigation and default components.

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AccountComponent } from './account/account.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path: '', component: DashboardComponent },
+   { path: 'account', component: AccountComponent }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    DashboardComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule,
