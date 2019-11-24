7d2829252af6faee5214c8f71be0a4cd725424a5
Joshua Belden <jcbelden@procaresoftware.com>
2019-11-24 14:43:58 -0800

    Account page shows basic information.

--- src/client/src/app/account/account.component.ts
+++ src/client/src/app/account/account.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticatedUser } from '../models/authenticatedUser';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-account',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountComponent implements OnInit {
 
-  constructor() { }
+  authenticatedUser: AuthenticatedUser;
+
+  constructor(authService: AuthService) {
+    this.authenticatedUser = authService.getAuthenticatedUser();
+  }
 
   ngOnInit() {
   }
-
 }
